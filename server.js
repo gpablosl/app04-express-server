9b5947b32e778923760b13a82558f68ce8c0e284
gpablosl <gpablosleon15@gmail.com>
2020-09-04 10:44:27 -0700

    Get and Post

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const port = 5000;
 
-app.use('/',(request,response)=>{
-    response.send('<h1>Hello my custom server</h1>')
+app.use(bodyParser.json());//cosumir cosas
+
+const tacos = [
+    {
+        id: 1,
+        name: 'de asada',
+        quantity: 5,
+        pica: 'no'
+    },
+    {
+        id: 2,
+        name: 'al pastor',
+        quantity: 4,
+        pica: 'si'
+    },
+    {
+        id: 3,
+        name: 'cabeza',
+        quantity: 6,
+        pica: 'no'
+    }
+];
+
+//Metodo GET o Obtener
+app.get('/', (request, response) =>{
+    response.send(tacos);
+});
+
+//agregar taco
+app.post('/', (request, response) =>{
+    const taco = request.body;
+    const {name, quantity, pica} = request.body;
+    taco.id = tacos.length + 1;
+    tacos.push(taco);
+    response.send(taco);
 });
 
 app.listen(port, ()=> console.log(`Server started at: http://localhost:${port}`));
